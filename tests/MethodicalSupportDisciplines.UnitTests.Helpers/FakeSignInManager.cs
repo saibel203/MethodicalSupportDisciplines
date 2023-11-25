d34f1546e1bc75f32bf989109dfb0cf355145f3c
saibel203 <saibl.og203@gmail.com>
2023-11-25 23:25:11 +0200

    Unit tests foundation

---
+++ tests/MethodicalSupportDisciplines.UnitTests.Helpers/FakeSignInManager.cs
@@ -0,0 +1,13 @@
+﻿using MethodicalSupportDisciplines.Core.Models.Identity;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace MethodicalSupportDisciplines.UnitTests.Helpers;
+
+public class FakeSignInManager
+{
+    
+}
